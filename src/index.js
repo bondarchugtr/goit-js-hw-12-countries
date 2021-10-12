d46b337a9206a2ee4d3551629b70c90353e36595
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-10-13 02:03:48 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './sass/main.scss';
+import './js/fetchCountries.js';
+import refs from './js/refs.js';
+import { debounce } from 'lodash';
+const { inputForm, inputValue } = refs;
+
+import { error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import * as PNotifyCountdown from '@pnotify/countdown';
+defaultModules.set(PNotifyMobile, {});
+
+
+inputValue.addEventListener('input', debounce((onInputCountrys), 500))
+
+function onInputCountrys(evt) {
+    console.log(evt)
+}
+
+
+
+
---
+++ src/js/refs.js
@@ -0,0 +1,4 @@
+export default {
+    inputForm: document.querySelector('.form__country'),
+    inputValue: document.querySelector('.form__country-input'),
+}
