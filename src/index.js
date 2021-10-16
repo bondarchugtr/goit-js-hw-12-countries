b4ad7ff286507ed950711f60e0f76ac6e645ae72
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-10-16 22:17:22 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'animate.css';
 import './sass/main.scss';
 import fetchCountries from './js/fetchCountries.js';
 import { debounce } from 'lodash';
@@ -8,7 +9,6 @@ import '@pnotify/core/dist/BrightTheme.css';
 import * as PNotifyCountdown from '@pnotify/countdown';
 
 defaultModules.set(PNotifyMobile, { swipeDismiss: false });
-
 import countrysElem from './js/templates/countrysElem.hbs';
 import countryList from './js/templates/countryList.hbs';
 
@@ -16,7 +16,6 @@ const refs = {
     inputForm: document.querySelector('.form__country'),
     inputValue: document.querySelector('.form__country-input'),
     divContainer: document.querySelector('.countrys__block'),
-    divContainer: document.querySelector('.countrys__block')
 }
 
 refs.inputValue.addEventListener('input', debounce((onInputCountrys), 500));
@@ -25,18 +24,24 @@ function countryGet(name) {
     fetchCountries(name)
         .then(data => {
             if (data.length > 10) {
-                error({ title: 'Too many matches found.', text: 'Please entera morespecific query!' });
-
+                errorPopUpList();
             } else if (data.length > 1) {
                 renderCountriesList(data);
-            } else {
+            } else if (countryCard !== 'null') {
+                const countryCard = document.querySelector('.country__option')
+                console.log(countryCard)
                 renderCountries(data);
             }
         })
         .catch(() => {
-            refs.divContainer.innerHTML = '';
-            error({ title: 'No information' });
+            errorPopUp()
         });
+}
+function errorPopUp() {
+    error({ title: 'Вы ввели не корректные данные!', text: 'Пожалуйста, введите название страны на Английском языке!' });
+}
+function errorPopUpList() {
+    error({ title: 'Найдено слишком много совпадений.', text: 'Пожалуйста, введите более конкретный запрос!' });
 }
 
 function onInputCountrys(evt) {
@@ -44,12 +49,11 @@ function onInputCountrys(evt) {
     countryGet(name)
 }
 
-
-
 function renderCountries(countries) {
     const marcup = countrysElem(countries);
     refs.divContainer.innerHTML = marcup;
 }
+
 
 function renderCountriesList(countries) {
     const marcup = countryList(countries);
@@ -69,14 +73,6 @@ function onClickLink() {
     })
 }
 
-
-export function errorNot(error) {
-    const myError = notice({
-        title: 'Error',
-        text: 'error',
-        modules: new Map([...defaultModules, [PNotifyDesktop, {}]]),
-    });
-}
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,9 @@
-import { notice, defaultModules } from '@pnotify/core';
-import * as PNotifyDesktop from '@pnotify/desktop';
-import '@pnotify/core/dist/PNotify.css';
-import '@pnotify/core/dist/BrightTheme.css';
-import { errorNot } from '../index.js'
+
+// import { errorNot } from '../index.js'
 export default function fetchCountries(name) {
     return fetch(`https://restcountries.com/v2/name/${name}`)
         .then(response => {
-            if (response.status !== 400) {
+            if (response.status !== 404) {
                 return response.json();
             }
         }).catch(error => alert(errorNot));
